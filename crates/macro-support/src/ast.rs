44e00f1b17b409d8cd8f512a29acef6e3e16bb7e
Joe Roberts <joe@jwjr.co.uk>
2019-08-27 07:18:48 +0100

    Handle  generating  array storage  more sanely

--- crates/macro-support/src/ast.rs
+++ crates/macro-support/src/ast.rs
@@ -1,7 +1,54 @@
-use proc_macro2::{TokenStream, Literal, Ident};
-use quote::{quote, format_ident, ToTokens};
+use proc_macro2::{TokenStream, Literal, Ident, Punct, Spacing, Group, Delimiter};
+use quote::{quote, format_ident, ToTokens, TokenStreamExt};
 
 use dotnet_bindgen_core::*;
+
+struct AstArray<T> {
+    ident: Ident,
+    values: Vec<T>,
+}
+
+// TODO: Could this actually be generic? clearly the following isn't the way to
+// go if many/any other array types are needed.
+impl AstArray<AstMethodArgument> {
+    fn new(name: &str, values: Vec<AstMethodArgument>) -> Self {
+        let ident = format_ident!("{}", name);
+        AstArray { ident, values }
+    }
+
+    fn storage_tokens(&self) -> TokenStream {
+        let ident = self.ident.clone();
+        let len = self.values.len();
+
+        let mut tokens = quote! {
+            #[link_section = #BINDGEN_DATA_SECTION_NAME]
+            static #ident: [::dotnet_bindgen_core::MethodArgument; #len] =
+        };
+
+        let mut values_tokens = TokenStream::new();
+        for element in &self.values {
+            values_tokens.extend(element.to_token_stream());
+            values_tokens.append(Punct::new(',', Spacing::Alone));
+        }
+        tokens.append(Group::new(Delimiter::Bracket, values_tokens));
+        tokens.append(Punct::new(';', Spacing::Alone));
+
+        for value in &self.values {
+            tokens.extend(value.storage_tokens());
+        }
+
+        tokens
+    }
+
+    fn reference_tokens(&self) -> TokenStream {
+        let ident = self.ident.clone();
+        quote! {
+            ::dotnet_bindgen_core::MaybeOwnedArr::Ref(
+                &#ident,
+            )
+        }
+    }
+}
 
 struct ByteString {
     ident: Ident,
@@ -17,20 +64,25 @@ impl ByteString {
         let value = Literal::byte_string(bytes);
         Self { ident, value, len }
     }
-}
 
-impl ToTokens for ByteString {
-    fn to_tokens(&self, tokens: &mut TokenStream) {
-        let ident = &self.ident;
-        let value = &self.value;
+    fn storage_tokens(&self) -> TokenStream {
+        let ident = self.ident.clone();
+        let value = self.value.clone();
         let len = self.len;
 
-        let new_tokens = quote! {
+        quote! {
             #[link_section = #BINDGEN_DATA_SECTION_NAME]
             static #ident: [u8; #len] = *#value;
-        };
+        }
+    }
 
-        tokens.extend(new_tokens);
+    fn reference_tokens(&self) -> TokenStream {
+        let ident = self.ident.clone();
+        quote! {
+            ::dotnet_bindgen_core::MaybeOwnedString {
+                bytes: ::dotnet_bindgen_core::MaybeOwnedArr::Ref(&#ident),
+            }
+        }
     }
 }
 
@@ -67,6 +119,10 @@ impl AstMethodArgument {
     fn name(&self) -> ByteString {
         ByteString::new(&format!("arg_{}_name", self.index), &self.source.name)
     }
+
+    fn storage_tokens(&self) -> TokenStream {
+        self.name().storage_tokens()
+    }
 }
 
 impl ToTokens for AstMethodArgument {
@@ -97,14 +153,24 @@ impl AstFunction {
         ByteString::new("func_name", &self.source.name)
     }
 
-    fn arguments<'a>(&'a self) -> impl Iterator<Item=AstMethodArgument> + 'a {
-        self.source.args
+    fn arguments(&self) -> AstArray<AstMethodArgument> {
+        let ast_arguments = self.source.args
             .iter()
             .enumerate()
             .map(|(i, arg)| AstMethodArgument {
                 source: arg.clone(),
                 index: i
             })
+            .collect();
+
+        AstArray::new("arguments", ast_arguments)
+    }
+
+    fn storage_tokens(&self) -> TokenStream {
+        let mut tokens = TokenStream::new();
+        tokens.extend(self.name().storage_tokens());
+        tokens.extend(self.arguments().storage_tokens());
+        tokens
     }
 }
 
@@ -116,43 +182,20 @@ impl From<BindgenFunction<'static>> for AstFunction {
 
 impl ToTokens for AstFunction {
     fn to_tokens(&self, tokens: &mut TokenStream) {
-        let mut strings = Vec::new();
-        let mut arg_chunks = Vec::new();
-        for arg in self.arguments() {
-            strings.push(arg.name());
-            arg_chunks.push(arg.to_token_stream());
-        }
-
         let mut inner_tokens = TokenStream::new();
-        for string in strings {
-            inner_tokens.extend(string.to_token_stream());
-        }
 
-        let arg_count = self.source.args.len();
-        let only_arg = arg_chunks.first().unwrap();
-        inner_tokens.extend(quote! {
-            #[link_section = #BINDGEN_DATA_SECTION_NAME]
-            pub static method_args: [::dotnet_bindgen_core::MethodArgument; #arg_count] = [
-                #only_arg
-            ];
-        });
+        inner_tokens.extend(self.storage_tokens());
 
-        let func_name = self.name();
-        let func_name_ident = self.name().ident;
-
+        let func_name_reference = self.name().reference_tokens();
+        let arguments_reference = self.arguments().reference_tokens();
         let return_type = AstType { source: self.source.return_type.clone() };
-
         let exposed_ident = format_ident!("__bindgen_func_{}", &self.source.name as &str);
         inner_tokens.extend(quote! {
-            #func_name
-
             #[no_mangle]
             #[link_section = #BINDGEN_SECTION_NAME]
             pub static #exposed_ident: ::dotnet_bindgen_core::BindgenFunction = ::dotnet_bindgen_core::BindgenFunction {
-                name: ::dotnet_bindgen_core::MaybeOwnedString {
-                    bytes: ::dotnet_bindgen_core::MaybeOwnedArr::Ref(&#func_name_ident),
-                },
-                args: ::dotnet_bindgen_core::MaybeOwnedArr::Ref(&method_args),
+                name: #func_name_reference,
+                args: #arguments_reference,
                 return_type: #return_type,
             };
         });
