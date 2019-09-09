5ae8995287f4f7c1b470135be9c4717a9ac2c15e
Joe Roberts <joe@jwjr.co.uk>
2019-09-09 08:03:44 +0100

    Update c# codegen for new descriptors

--- crates/dotnet-bindgen-cli/src/codegen.rs
+++ crates/dotnet-bindgen-cli/src/codegen.rs
@@ -3,6 +3,7 @@ use std::path::{Path, PathBuf};
 
 use heck::CamelCase;
 
+use dotnet_bindgen_core as core;
 use crate::ast;
 use crate::data::BindgenData;
 
@@ -43,6 +44,54 @@ fn create_csproj(data: &BindgenData, project_dir: &Path) -> Result<(), std::io::
     Ok(())
 }
 
+/// Maps a BindgenTypeDescriptor to 
+fn map_descriptor_to_thunk_type(descriptor: &core::BindgenTypeDescriptor) -> ast::CSharpType {
+    use dotnet_bindgen_core::BindgenTypeDescriptor as Desc;
+    use ast::CSharpType as CS;
+    match descriptor {
+        Desc::Void => CS::Void,
+        Desc::Int { width: 8, signed: true } => CS::SByte,
+        Desc::Int { width: 16, signed: true } => CS::Int16,
+        Desc::Int { width: 32, signed: true } => CS::Int32,
+        Desc::Int { width: 64, signed: true } => CS::Int64,
+        Desc::Int { width: 8, signed: false } => CS::Byte,
+        Desc::Int { width: 16, signed: false } => CS::UInt16,
+        Desc::Int { width: 32, signed: false } => CS::UInt32,
+        Desc::Int { width: 64, signed: false } => CS::UInt64,
+        Desc::Slice { elem_type: _ } => CS::Struct { name: ast::Ident("SliceAbi".to_string()) },
+        _ => panic!("Untranslatable type")
+    }
+}
+
+pub fn func_descriptor_to_imported_method(
+    binary_name: &str,
+    descriptor: &core::BindgenFunctionDescriptor
+) -> ast::Method {
+
+    let attributes = vec![
+        ast::Attribute::dll_import(binary_name, &descriptor.thunk_name)
+    ];
+    let name = descriptor.thunk_name.to_camel_case();
+    let return_ty = map_descriptor_to_thunk_type(&descriptor.return_ty);
+    let args = descriptor.arguments
+        .iter()
+        .map(|arg| ast::MethodArgument {
+            name: ast::Ident(arg.name.to_string()),
+            ty: map_descriptor_to_thunk_type(&arg.ty)
+        }).collect();
+
+    ast::Method {
+        attributes,
+        is_public: false,
+        is_static: true,
+        is_extern: true,
+        name,
+        return_ty,
+        args,
+        body: None,
+    }
+}
+
 pub fn create_project(data: crate::data::BindgenData, project_dir: PathBuf) -> Result<(), ()> {
     create_csproj(&data, &project_dir).unwrap();
 
@@ -52,6 +101,14 @@ pub fn create_project(data: crate::data::BindgenData, project_dir: PathBuf) -> R
         .expect("Expect a filename in the path".into())
         .to_str()
         .expect("Expec the filename to be valid unicode");
+
+    let mut methods = Vec::new();
+
+    for descriptor in data.descriptors {
+        methods.push(
+            func_descriptor_to_imported_method(binary_name, &descriptor)
+        );
+    }
 
     // For the first pass, stick everything in one file
     let root = ast::Root {
@@ -71,10 +128,7 @@ pub fn create_project(data: crate::data::BindgenData, project_dir: PathBuf) -> R
             children: vec![Box::new(ast::Class {
                 name: "Imports".into(),
                 is_static: true,
-                methods: vec![ast::ImportedMethod {
-                    binary_name: binary_name.into(),
-                    func_data: data.get_function().clone(),
-                }],
+                methods
             })],
         })],
     };
